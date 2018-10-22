e8465c1f9ba80ab1b35cb5efdf0bc49b1c79f0c4
Bobermen <olegich051@gmail.com>
2018-10-22 23:19:32 +0300

    Управление камерой

--- src/game/Camera.java
+++ src/game/Camera.java
@@ -1,5 +1,10 @@
 package game;
 
+import linAlg.Vector2.Vector2;
+
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
 public class Camera extends Component {
 
     public static Camera main = null;
@@ -21,6 +26,23 @@ public class Camera extends Component {
         }
         if (width < 50) {
             width = 50;
+        }
+
+        if (Input.isKey(KeyEvent.VK_UP)) {
+            transform.setLocalPosition(transform.getLocalPosition()
+                    .add(Vector2.getVector2(0, 100*Time.detlaTime)));
+        }
+        if (Input.isKey(KeyEvent.VK_DOWN)) {
+            transform.setLocalPosition(transform.getLocalPosition()
+                    .add(Vector2.getVector2(0, -100*Time.detlaTime)));
+        }
+        if (Input.isKey(KeyEvent.VK_RIGHT)) {
+            transform.setLocalPosition(transform.getLocalPosition()
+                    .add(Vector2.getVector2(100*Time.detlaTime, 0)));
+        }
+        if (Input.isKey(KeyEvent.VK_LEFT)) {
+            transform.setLocalPosition(transform.getLocalPosition()
+                    .add(Vector2.getVector2(-100*Time.detlaTime, 0)));
         }
     }
 }
