07f8ddc9058bd315278ed6295dc3323cbb5d0148
Bobermen <olegich051@gmail.com>
2018-10-26 14:07:46 +0300

    Скорость камеры зависит от масштаба

--- src/game/Camera.java
+++ src/game/Camera.java
@@ -10,17 +10,28 @@ public class Camera extends Component {
     public static Camera main = null;
     public double width = 1000, height;
     public int resolutionWidth, resolutionHeight;
-    public Camera (int resolutionWidth, int resolutionHeight) {
-        this.resolutionWidth = resolutionWidth;
-        this.resolutionHeight = resolutionHeight;
-        height = width/resolutionWidth * resolutionHeight;
+    public double cameraSpeed = 0.1;
+    public double cameraScroll = 0.1;
+
+
+    public Camera () {
         main = this;
     }
 
     // TODO posChange, screenToWorld, worldToScreen
-    public void update() {
+
+    @Override
+    public void start() {
+        this.resolutionWidth = resolutionWidth;
+        this.resolutionHeight = resolutionHeight;
         height = width/resolutionWidth * resolutionHeight;
-        width += Input.getScroll() * 100;
+    }
+    @Override
+    public void update() {
+        resolutionWidth = Game.frame.getWidth();
+        resolutionHeight = Game.frame.getHeight();
+        height = width/resolutionWidth * resolutionHeight;
+        width += Input.getScroll() * width * cameraScroll;
         if (width > 40000) {
             width = 40000;
         }
@@ -30,19 +41,19 @@ public class Camera extends Component {
 
         if (Input.isKey(KeyEvent.VK_UP)) {
             transform.setLocalPosition(transform.getLocalPosition()
-                    .add(Vector2.getVector2(0, 100*Time.detlaTime)));
+                    .add(Vector2.getVector2(0, cameraSpeed*width*Time.detlaTime)));
         }
         if (Input.isKey(KeyEvent.VK_DOWN)) {
             transform.setLocalPosition(transform.getLocalPosition()
-                    .add(Vector2.getVector2(0, -100*Time.detlaTime)));
+                    .add(Vector2.getVector2(0, -cameraSpeed*width*Time.detlaTime)));
         }
         if (Input.isKey(KeyEvent.VK_RIGHT)) {
             transform.setLocalPosition(transform.getLocalPosition()
-                    .add(Vector2.getVector2(100*Time.detlaTime, 0)));
+                    .add(Vector2.getVector2(cameraSpeed*width*Time.detlaTime, 0)));
         }
         if (Input.isKey(KeyEvent.VK_LEFT)) {
             transform.setLocalPosition(transform.getLocalPosition()
-                    .add(Vector2.getVector2(-100*Time.detlaTime, 0)));
+                    .add(Vector2.getVector2(-cameraSpeed*width*Time.detlaTime, 0)));
         }
     }
 
